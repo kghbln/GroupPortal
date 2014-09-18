69eaa43f455d517cd1ffbb19de6f5bbb0deab2f5
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-09-18 22:23:12 +0200

    Update GroupPortal.php
    Fix for depreciated spliti() function / Thanks go to [Davydog](https://www.mediawiki.org/wiki/Thread:Extension_talk:GroupPortal/spliti()_is_deprecated)

--- GroupPortal.php
+++ GroupPortal.php
@@ -1,46 +1,57 @@
 <?php
+// Ensure that the script cannot be executed outside of MediaWiki.
+if ( !defined( 'MEDIAWIKI' ) ) {
+    die( 'This is an extension to the MediaWiki package and cannot be run standalone.' );
+}
 
+// Display extension properties on MediaWiki.
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
-	'name'    => "GroupPortal",
-	'version' => '1.0',
-	'author'  => 'Tim Laqua',
-	'url'     => 'https://www.mediawiki.org/wiki/Extension:GroupPortal'
-);
+	'name' => 'GroupPortal',
+	'author' => array(
+		'Tim Laqua',
+		'...'
+		),
+	'url' => 'https://www.mediawiki.org/wiki/Extension:GroupPortal',
+	'description' => 'Allows for group-based main page redirection',
+	'version' => '1.1.0'
+	);
 
+// Register extension class.
 $wgHooks['MediaWikiPerformAction'][] = 'efGroupPortal_MediaWikiPerformAction';
 
+// Doing all the action.
 function efGroupPortal_MediaWikiPerformAction( $output, $article, $title, $user, $request ) {
-	$action    = $request->getVal( 'action', 'view' );
-	$redirect  = $request->getVal( 'redirect' );
+        $action    = $request->getVal( 'action', 'view' );
+        $redirect  = $request->getVal( 'redirect' );
 
-	if ( $action === 'view' && $redirect === null ) {
-		if ( $title->equals( Title::newMainPage() ) ) {
-			$groupPortals = spliti( "\n", wfMsgForContentNoTrans( 'groupportal' ) );
+        if ( $action === 'view' && $redirect === null ) {
+                if ( $title->equals( Title::newMainPage() ) ) {
+                        $groupPortals = explode( "\n", wfMsgForContentNoTrans( 'groupportal' ) );
 
-			$groups = $user->getGroups();
+                        $groups = $user->getGroups();
 
-			$targetPortal = '';
-			foreach ( $groupPortals as $groupPortal ) {
-				$mcount = preg_match( '/^(.+)\|(.+)$/', $groupPortal, $matches );
+                        $targetPortal = '';
+                        foreach ( $groupPortals as $groupPortal ) {
+                                $mcount = preg_match( '/^(.+)\|(.+)$/', $groupPortal, $matches );
 
-				if ( $mcount > 0 ) {
-					if ( in_array($matches[1], $groups ) ||
-						 ( $matches[1] == '*' && empty( $targetPortal ) ) ) {
-						$targetPortal = $matches[2];
-					}
-				}
-			}
+                                if ( $mcount > 0 ) {
+                                        if ( in_array( $matches[1], $groups ) ||
+                                                ( $matches[1] == '*' && empty( $targetPortal ) ) ) {
+                                                	$targetPortal = $matches[2];
+                                        }
+                                }
+                        }
 
-			if ( !empty( $targetPortal ) ) {
-				$target = Title::newFromText( $targetPortal );
+                        if ( !empty( $targetPortal ) ) {
+                                $target = Title::newFromText( $targetPortal );
 
-				if( is_object( $target ) ) {
-					$output->redirect( $target->getLocalURL() );
-					return false;
-				}
-			}
-		}
-	}
-	return true;
+                                if( is_object( $target ) ) {
+                                        $output->redirect( $target->getLocalURL() );
+                                        return false;
+                                }
+                        }
+                }
+        }
+        return true;
 }
